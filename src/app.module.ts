d1204befdaad5f2d0215e4af79a99a070b49bff8
Hasan Goli <hasangolidev@gmail.com>
2024-02-25 11:47:37 +0330

    Coffees and users resources generated and database connection configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ep-delicate-pine-a58ibk4a.us-east-2.aws.neon.tech',
+      port: 5432,
+      username: 'hasangoli',
+      password: 'Qwrsp1XvDda7',
+      database: 'nest-auth',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+    }),
+    CoffeesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
