8c2270ca21cd6cc3dc6708d7255897171ef72dab
Hasan Goli <hasangolidev@gmail.com>
2024-06-20 23:02:31 +0330

    Typeorm and docker configured and user entity added to users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
