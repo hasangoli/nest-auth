03be546a0607c5afd2e1ec229fa2b533cfd82fd3
Hasan Goli <hasangolidev@gmail.com>
2024-06-20 23:40:47 +0330

    Auth routes configured

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthenticationService,
   ],
+  controllers: [AuthenticationController],
 })
 export class IamModule {}
