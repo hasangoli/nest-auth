03ed34d9d41f217d734c217309274663e297f269
Hasan Goli <hasangolidev@gmail.com>
2024-06-21 21:55:51 +0330

    Role based authorization implemented

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,8 +7,10 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
 import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
+import { Role } from 'src/users/enums/role.enum';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -17,6 +19,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
@@ -33,11 +36,13 @@ export class CoffeesController {
     return this.coffeesService.findOne(+id);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(+id, updateCoffeeDto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coffeesService.remove(+id);
