b134db104c1a884adc0d4f887f4ef67ff0b810da
Hasan Goli <hasangolidev@gmail.com>
2024-06-21 12:08:47 +0330

    Global guard configured

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AuthenticationService } from './authentication.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -13,24 +21,26 @@ export class AuthenticationController {
   }
 
   // Sends jwt token in response body
-  @HttpCode(HttpStatus.OK)
-  @Post('sign-in')
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authenticationService.signIn(signInDto);
-  }
-
-  // Sends jwt token in header cookie
   // @HttpCode(HttpStatus.OK)
   // @Post('sign-in')
-  // async signIn(
-  //   @Res({ passthrough: true }) response: Response,
-  //   @Body() signInDto: SignInDto,
-  // ) {
-  //   const accessToken = await this.authenticationService.signIn(signInDto);
-  //   response.cookie('accessToken', {
-  //     secure: true,
-  //     httpOnly: true,
-  //     sameSite: true,
-  //   });
+  // signIn(@Body() signInDto: SignInDto) {
+  //   return this.authenticationService.signIn(signInDto);
   // }
+
+  // Sends jwt token in header cookie
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  async signIn(
+    @Res({ passthrough: true }) response: Response,
+    @Body() signInDto: SignInDto,
+  ) {
+    const accessToken = await this.authenticationService.signIn(signInDto);
+    response.cookie('accessToken', {
+      secure: true,
+      httpOnly: true,
+      sameSite: true,
+    });
+
+    return { accessToken };
+  }
 }
