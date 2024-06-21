2a4a4ba2921fe7631f160ec5a637506e3e1e2595
Hasan Goli <hasangolidev@gmail.com>
2024-06-21 22:41:48 +0330

    Authorization based on permissions added

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,10 +7,10 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
+import { Permissions } from 'src/iam/authorization/decorators/permissions.decorator';
+import { Permission } from 'src/iam/authorization/permission.type';
 import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
-import { Role } from 'src/users/enums/role.enum';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -19,7 +19,8 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
-  @Roles(Role.Admin)
+  // @Roles(Role.Admin)
+  @Permissions(Permission.CreateCoffee)
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
@@ -36,13 +37,15 @@ export class CoffeesController {
     return this.coffeesService.findOne(+id);
   }
 
-  @Roles(Role.Admin)
+  // @Roles(Role.Admin)
+  @Permissions(Permission.UpdateCoffee)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(+id, updateCoffeeDto);
   }
 
-  @Roles(Role.Admin)
+  // @Roles(Role.Admin)
+  @Permissions(Permission.DeleteCoffee)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coffeesService.remove(+id);
---
+++ src/iam/authorization/permission.type.ts
@@ -0,0 +1,5 @@
+import { CoffeesPermissions } from 'src/coffees/coffees.permissions';
+
+export const Permission = { ...CoffeesPermissions };
+
+export type PermissionType = CoffeesPermissions;
