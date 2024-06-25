5ba6c57a8a577b70923317a4a5e4afdaf436ce30
Hasan Goli <hasangolidev@gmail.com>
2024-06-25 20:44:03 +0330

    Policy-based authorization implemented

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,7 +8,9 @@ import {
   Post,
 } from '@nestjs/common';
 import { Permissions } from 'src/iam/authorization/decorators/permissions.decorator';
+import { Policies } from 'src/iam/authorization/decorators/policies.decorator';
 import { Permission } from 'src/iam/authorization/permission.type';
+import { FrameworkContributorPolicy } from 'src/iam/authorization/policies/framework-contributor.policy';
 import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 import { CoffeesService } from './coffees.service';
@@ -20,7 +22,10 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   // @Roles(Role.Admin)
-  @Permissions(Permission.CreateCoffee)
+  // @Permissions(Permission.CreateCoffee)
+  @Policies(
+    new FrameworkContributorPolicy() /** new MinAgePolicy(18), new OnlyAdminPolicy() */,
+  )
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
